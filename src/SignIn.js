696a8ab07f25f92e7cc87daa15355e9e51f303b6
ShortCoding <bouthoukine.ayoub@gmail.com>
2017-10-19 15:20:58 +0200

    Push

--- src/SignIn.js
+++ src/SignIn.js
@@ -4,7 +4,7 @@ const SignIn = React.createClass({
 
   signIn() {
     // firebase.google.com/docs/auth/web/github-auth#handle_the_sign-in_flow_with_the_firebase_sdk
-    const provider = new firebase.auth.GoogleAuthProvider(); // eslint-disable-line no-undef
+    const provider = new firebase.auth.GithubAuthProvider(); // eslint-disable-line no-undef
     firebase.auth().signInWithPopup(provider).then((result) => { // eslint-disable-line no-undef
       const user = result.user;
       firebase.database().ref(`users/${user.uid}`).set({ // eslint-disable-line no-undef
